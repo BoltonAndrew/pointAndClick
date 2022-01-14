f77b7c8f0632a87ac35d25b9b151f818d8e1bff4
Andrew Bolton <boltonandrew@live.co.uk>
2022-01-14 22:54:17 +0000

    collision mapping complete

--- src/App.js
+++ src/App.js
@@ -29,40 +29,37 @@ const App = () => {
 
   const rightClick = async (e, box) => {
     e.preventDefault();
-
-    // setDifference(tempNum);
-    // console.log(difference);
-    // setSpeed({ speed: 0.6 * difference, steps: 20 * difference });
-    if (yPosition > box.yLocation) {
-      setDirection("left");
-    } else {
-      setDirection("right");
+    if (box.move) {
+      if (yPosition > box.yLocation) {
+        setDirection("left");
+      } else {
+        setDirection("right");
+      }
+      let tempNum;
+      if (yPosition > box.yLocation) {
+        tempNum = tile - box.tile;
+      } else {
+        tempNum = box.tile - tile;
+      }
+      console.log(tempNum);
+      const tempSpeed = 0.6 * tempNum;
+      const tempSteps = 20 * tempNum;
+      setSpeed(tempSpeed);
+      setSteps(tempSteps);
+      setXDestination(box.xLocation);
+      setYDestination(box.yLocation);
+      setWalk(true);
+      setTile(box.tile);
+      setTimeout(() => {
+        setWalk(false);
+        setXPosition(box.xLocation);
+        setYPosition(box.yLocation);
+        setSpeed(0.6);
+        setSteps(20);
+        setXDestination();
+        setYDestination();
+      }, 600 * tempNum - 20);
     }
-    let tempNum;
-    if (yPosition > box.yLocation) {
-      tempNum = tile - box.tile;
-    } else {
-      tempNum = box.tile - tile;
-    }
-    console.log(tempNum);
-    const tempSpeed = 0.6 * tempNum;
-    const tempSteps = 20 * tempNum;
-    setSpeed(tempSpeed);
-    setSteps(tempSteps);
-    setXDestination(box.xLocation);
-    setYDestination(box.yLocation);
-    setWalk(true);
-    setTile(box.tile);
-    setTimeout(() => {
-      setWalk(false);
-      setXPosition(box.xLocation);
-      setYPosition(box.yLocation);
-      setSpeed(0.6);
-      setSteps(20);
-      setXDestination();
-      setYDestination();
-    }, 600 * tempNum - 20);
-    // alert("move");
   };
 
   return (
